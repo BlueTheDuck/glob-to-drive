5e7600c4b6b8921e11a5db05aec0b80bfb058aaa
BlueTheDuck <no-reply@perezv.ar>
2020-06-01 02:13:00 -0300

    Split main file into `drive-wrapper`

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const core = require("@actions/core");
 const { google } = require("googleapis");
-const fs = require("fs");
+const core = require("@actions/core");
 const glob = require("glob");
 const path = require("path");
+const { login, createDriveApi, list, update, upload } = require("./drive-wrapper.js");
+
 
 /**
  * These are the scopes. The full list is here: https://developers.google.com/identity/protocols/oauth2/scopes#drive
@@ -11,105 +12,7 @@ const path = require("path");
  * */
 const SCOPES = require("./scopes.json");
 
-//#region GApi
-async function login() {
-    try {
-        /**
-         * Get info required to perform the auth from the inputs vars
-         * credentials is a JSON value downloaded from the Google Console OAuth
-         * token can ve generated running `node login.js`. The file `token.json` will then contain the value
-         * UPLOAD THIS VALUES AS SECRETS! NEVER KEEP THEM UNENCRYPTED
-         */
-        const credentials = JSON.parse(core.getInput("credentials", { required: true }));
-        const token = JSON.parse(core.getInput("token", { required: true }));
-        let auth = new google.auth.OAuth2(
-            credentials.web.client_id,
-            credentials.web.client_secret,
-            credentials.web.redirect_uris[0]
-        )
-        auth.setCredentials(token);
-        core.info("Auth successful");
-        return auth;
-    } catch (e) {
-        return Promise.reject(e);
-    }
-}
-
-/**
- * @param {import("googleapis").drive_v3.Drive} drive Google Drive context
- */
-async function list(drive, q) {
-    let files = [];
-    let pageToken = undefined;
-    do {
-        let { data } = await drive.files.list({
-            pageSize: 10,
-            q,
-            pageToken
-        });
-        files.concat(data.files);
-        pageToken = data.nextPageToken;
-    } while (pageToken !== undefined);
-    await Promise.all(files.map(async file => {
-        const { data } = await drive.files.get({ fileId: file.id, fields: "appProperties" });
-        file.appProperties = data.appProperties;
-    }))
-
-    return files;
-}
-
-/**
- * @param {import("googleapis").drive_v3.Drive} drive Google Drive context
- * @param {object} options
- * @param options.path Path to the file that will be uploaded
- * @param options.name Name of the file in Drive. Defaults to `options.path` if left empty
- * @param options.mimeType Mimetype (Used both for media: and requestBody:)
- * @param options.parents Parent folder. If empty, the file will be uploaded to My Drive
- */
-async function upload(drive, options) {
-    // Patch parents, it has to be an array
-    if (!options.parents) {
-        options.parents = [];
-    } else if (typeof options.parents === 'string') {
-        options.parents = [options.parents]
-    }
-    core.info(`Uploading file ${options.path} with mime-type ${options.mimeType || "empty"}`);
-    try {
-        await drive.files.create({
-            requestBody: {
-                mimeType: options.mimeType,
-                name: options.name || options.path,
-                parents: options.parents,
-                appProperties: {
-                    source: options.path
-                }
-            },
-            media: {
-                mimeType: options.mimeType,
-                body: fs.createReadStream(options.path)
-            }
-        });
-    } catch (e) {
-        return Promise.reject(e);
-    }
-}
-
-/**
- * @param {import("googleapis").drive_v3.Drive} drive Google Drive context
- * @param {object} options
- * @param {string} options.fileId
- * @param options.file File to get the content from
- */
-async function update(drive, options) {
-    core.info(`Updating ${options.file}`);
-    await drive.files.update({
-        fileId: options.fileId,
-        media: {
-            body: fs.createReadStream(options.file)
-        }
-    })
-}
-//#endregion
+let Drive = null;
 
 //#region Helpers
 /**
@@ -146,18 +49,25 @@ async function getMatchedFiles() {
 }
 //#endregion
 
-let Drive = null;
+async function main() {
+    // Try to get the login info as Action Inputs
+    try {
+        const credentials = JSON.parse(core.getInput("credentials", { required: true }));
+        const token = JSON.parse(core.getInput("token", { required: true }));
+        return [credentials, token];
+    } catch (e) {
+        return Promise.reject(e);
+    }
+}
 
-// Perform auth
-login()
+main()
+    // Perform auth
+    .then(([credentials, token]) => login(credentials, token))
     .then(auth => {
-        // Create the Drive API and store it in the global var `drive`
-        Drive = google.drive({
-            version: "v3",
-            auth,
-        });
+        // Drive is the API, set as a global var
+        Drive = createDriveApi(auth);
     })
-    // Find files and get GDrive info
+    // Find get GDrive info and find files
     .then(_ => Promise.all([getGDriveFiles(Drive), getMatchedFiles()]))
     // Perform the upload/update
     .then(([gfiles, matches]) =>
